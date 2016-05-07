9dda89d4ebea9a6cd3896fa025256bb518fd9277
Peter McNeely <pete.mcneely@gmail.com>
2016-05-07 14:29:45 -0400

    Checkpoint 48: API Retrieving. Complete.

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,13 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
